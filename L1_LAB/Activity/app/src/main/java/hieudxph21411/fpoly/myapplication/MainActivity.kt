1f20bf30dd7129f2990ed594a962dba42fd601cb
hieudxx <dxhieu03@gmail.com>
2023-09-14 20:47:04 +0700

    first commit

---
+++ L1_LAB/Activity/app/src/main/java/hieudxph21411/fpoly/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package hieudxph21411.fpoly.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
